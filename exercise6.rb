84a386a003f03104f3dad00fef047beb773c55a2
geomese <geomese@gmail.com>
2013-10-22 15:42:43 -0400

    grocery shoppin

---
+++ exercise6.rb
@@ -0,0 +1,29 @@
+def grocery_list  
+  ["carrots", "toilet paper", "apples", "salmon"]
+end
+
+grocery_list.each do |x|
+  puts "* #{x}"
+end
+
+puts grocery_list.length
+
+if grocery_list.include? "bananas"
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas"
+end
+
+puts grocery_list[1]
+
+grocery_list.sort.each do |x|
+  puts "* #{x}"
+end
+
+
+grocery_list.sort.each do |x|
+  if x == "salmon"
+    delete
+  else
+    puts "* #{x}"
+end
