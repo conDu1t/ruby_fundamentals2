229204c74db0b07bb14d811907ae3223b7ef8faa
geomese <geomese@gmail.com>
2013-10-23 00:20:32 -0400

    bonus added

--- exercise7.rb
+++ exercise7.rb
@@ -10,12 +10,22 @@ end
 
 list(students)
 
+#adding cohort 4
+
 students[:cohort4] = 43
 
 puts students.keys
+
+#increase in each cohort by 5%
 
 students.each { |k, v| students[k] = (v * 1.05).ceil.to_i}
 list(students)
 
 students.delete(:cohort2)
 list(students)
+
+#BONUS adding classes together
+
+sum = 0
+students.each { |key, value| sum += value }
+puts "Total number of students who have enrolled with Bitmaker is: #{sum}"
